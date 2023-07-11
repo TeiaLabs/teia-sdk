38c04f1ee8393ff98c903bba72d71037348fc248
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2023-07-11 15:53:47 -0300

    update sdk search schema

--- teia_sdk/search/schemas.py
+++ teia_sdk/search/schemas.py
@@ -16,5 +16,16 @@ class SearchRequest(TypedDict):
     search_settings: list[SearchSettings]
 
 
-class SearchResponse(TypedDict):
-    pass
+class SearchResult(TypedDict):
+    content: str
+    data_type: str
+    file_id: str
+    kb_name: str
+    query: str
+    url: str
+    instance_id: str
+    relevance: int
+
+
+class Searchresponse(TypedDict):
+    results: list[SearchResult]
