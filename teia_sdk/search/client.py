b675ce37f862dd3398133f984f77d44184993e4b
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2023-07-11 10:27:38 -0300

    Adjust search client

--- teia_sdk/search/client.py
+++ teia_sdk/search/client.py
@@ -1,12 +1,13 @@
+import json
 import os
 import httpx
 
 try:
-    SEARCH_API_KEY = os.environ["SEARCH_API_KEY"]
+    TEIA_API_KEY = os.environ["TEIA_API_KEY"]
     SEARCH_API_URL = os.getenv("SEARCH_API_URL", "https://athena.teialabs.com.br:3232")
 except KeyError:
     m = "[red]MissingEnvironmentVariables[/red]: "
-    m += "[yellow]'SEARCH_API_KEY'[/yellow] cannot be empty."
+    m += "[yellow]'TEIA_API_KEY'[/yellow] cannot be empty."
     print(m)
     exit(1)
 
@@ -17,15 +18,15 @@ class SearchClient:
     @classmethod
     def get_headers(cls) -> dict[str, str]:
         obj = {
-            "Authorization": f"Bearer {SEARCH_API_KEY}",
+            "Authorization": f"Bearer {TEIA_API_KEY}",
         }
         return obj
 
     @classmethod
-    def search(cls, query: str) -> dict:
+    def search(cls, body: dict) -> dict:
         response = httpx.get(
             url=f"{SEARCH_API_URL}{cls.relativepath}",
             headers=cls.get_headers(),
-            params=query,
+            json=body,
         )
         return response.json()
