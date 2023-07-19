a1b2c83d31ab942e03d03554d3a4fbfae42b2f52
Jônatas Wehrmann <jonataswehrmann@gmail.com>
2023-07-19 10:04:03 -0300

    Separate select and run methods

--- teia_sdk/plugins/client.py
+++ teia_sdk/plugins/client.py
@@ -17,6 +17,9 @@ except KeyError:
 
 
 class PluginClient:
+
+    client = httpx.Client(timeout=60)
+
     @classmethod
     def get_headers(cls) -> dict[str, str]:
         obj = {
@@ -57,7 +60,7 @@ class PluginClient:
 
         sel_run_url = f"{PLUGINS_API_URL}/select-and-run-plugin"
         try:
-            plugins_data = httpx.post(
+            plugins_data = cls.client.post(
                 sel_run_url,
                 json=sp,
                 headers=cls.get_headers(),
@@ -89,7 +92,7 @@ class PluginClient:
     @classmethod
     def run_selector(
         cls, message: str, context: str, plugin_list: list[str], prompt_name: str
-    ):
+    ) -> PluginUsage:
         sp = SelectPlugin(
             prompt_name=prompt_name,
             current_message=message,
@@ -99,43 +102,34 @@ class PluginClient:
 
         plugin_host = PLUGINS_API_URL
         headers = cls.get_headers()
-        plugins_selected = httpx.post(
+
+        plugins_selected = cls.client.post(
             f"{plugin_host}/select-plugin",
             data=sp.json(),
             headers=headers,
         )
 
-        if plugins_selected.status_code != http_status.HTTP_200_OK:
-            return PluginResponse(
-                selector_completion="",
-                plugins_infos=[],
-                error=f"{plugins_selected.status_code}: {plugins_selected.text}",
-            )
-
+        plugins_selected.raise_for_status()
         print("plugins_selected", plugins_selected)
 
         plugin_calls = PluginUsage(**plugins_selected.json()["plugin_usage"][0])
-
-        plugin_payload = str([plugin_calls.dict()])
+        return plugin_calls
+    
+    @classmethod
+    def run_plugins(cls, plugin_calls: list[PluginUsage]):
+        plugin_payload = str(plugin_calls)
 
         body_data = {
             "plugin_selector_payload": plugin_payload,
         }
 
         plugin_data = httpx.post(
-            f"{plugin_host}/run-plugin",
+            f"{PLUGINS_API_URL}/run-plugin",
             params=body_data,
-            headers=headers,
+            headers=cls.get_headers(),
         )
 
-        print(plugin_data)
-
-        if plugins_selected.status_code != http_status.HTTP_200_OK:
-            plugin_data = PluginResponse(
-                selector_completion=plugins_selected,
-                error=f"{plugin_data.status_code}: {plugin_data.text}",
-            )
-            return plugin_data
+        plugin_data.raise_for_status()
 
         plugin_data = plugin_data.json()
         plugin_data = PluginResponse(**plugin_data)
