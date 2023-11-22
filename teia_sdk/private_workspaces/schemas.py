39e8f3ea4f21f120a413397dea54f1f17cff6364
kristen <kristenarguello20@gmail.com>
2023-11-22 16:46:50 -0300

    Add schemas for private workspaces client

--- teia_sdk/private_workspaces/schemas.py
+++ teia_sdk/private_workspaces/schemas.py
@@ -1,5 +1,6 @@
 import datetime
 from typing import NotRequired, TypedDict
+from pathlib import Path
 
 from tauth.schemas import Creator
 
@@ -18,3 +19,40 @@ class PrivateWorkspaceCreationResponse(PrivateWorkspaceCreationRequest):
     created_at: datetime
     created_by: Creator
     workspace_id: str
+
+
+class PrivateWorkspace(TypedDict):
+    id: str
+    created_by: Creator
+    name: str
+    indexing_settings: IndexingSettings
+
+
+class PrivateFile(TypedDict):
+    created_by: Creator
+    workspace_id: str
+    filepath: Path | str
+    filename: str
+    filetype: str
+    content: NotRequired[str]
+    content_hash: NotRequired[str]
+    content_type: NotRequired[str]
+    media_type: NotRequired[str]
+    size_bytes: int
+
+
+class OperationExecution(TypedDict):
+    created_at: datetime
+    message: NotRequired[str]
+    status: str
+
+
+class IndexingOperation(TypedDict):
+    type: str
+    executions: NotRequired[list[OperationExecution]]
+
+
+class PrivateWorkspaceIndexing(TypedDict):
+    id: str
+    workspace_id: str
+    operations: NotRequired[list[IndexingOperation]]
